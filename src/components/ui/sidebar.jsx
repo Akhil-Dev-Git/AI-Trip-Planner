1b4e297d581a3e93dfc2f89423811b2b4dc9aeaa
Akhil Dev <dev897330@gmail.com>
2026-08-21 00:11:39 +0530

    Fix mobile sidebar layout to use fixed transform off-canvas

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -27,10 +27,8 @@ const Sidebar = React.forwardRef(({ className, ...props }, ref) => {
       <div
         ref={ref}
         className={cn(
-          'h-full w-64 flex-col border-r bg-background',
-          isOpen 
-            ? 'fixed inset-y-0 left-0 z-50 flex shadow-2xl' 
-            : 'hidden md:flex md:static',
+          'fixed inset-y-0 left-0 z-50 flex h-full w-64 flex-col border-r bg-background transition-transform duration-300 md:static md:translate-x-0',
+          isOpen ? 'translate-x-0 shadow-2xl' : '-translate-x-full',
           className
         )}
         {...props}
